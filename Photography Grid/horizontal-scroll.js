30cffa60ca9f4280d6d984bdc37d9ba342988666
biscu <biscu@live.it>
2021-09-03 13:55:09 +0200

    Delete Photography Grid directory

--- Photography Grid/horizontal-scroll.js
+++
@@ -1,8 +0,0 @@
-let locoScrollDefaults = {
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true,
-    direction: "horizontal",
-};
-
-let locoScroll = new LocomotiveScroll(locoScrollDefaults);
-
