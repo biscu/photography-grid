fafff94c27a4525a749cb8bff0aaebc0934f7f81
biscu <biscu@live.it>
2021-09-03 13:54:59 +0200

    Add files via upload

---
+++ horizontal-scroll.js
@@ -0,0 +1,8 @@
+let locoScrollDefaults = {
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    direction: "horizontal",
+};
+
+let locoScroll = new LocomotiveScroll(locoScrollDefaults);
+
